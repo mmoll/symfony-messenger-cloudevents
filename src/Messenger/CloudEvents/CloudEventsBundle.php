09f93014c663d442712d591bd3f0c4224eaea82e
Rene Terstegen <rene@reneterstegen.eu>
2025-08-08 13:41:32 +0200

    Added first bundle integration

---
+++ src/Messenger/CloudEvents/CloudEventsBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Stegeman\Messenger\CloudEvents;
+
+use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+class CloudEventsBundle extends AbstractBundle
+{
+    public function configure(DefinitionConfigurator $definition): void
+    {
+
+    }
+}
