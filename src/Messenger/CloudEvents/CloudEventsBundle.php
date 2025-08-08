f96c804efc7a24a31440a3ee016fe121c6e95d86
Rene Terstegen <rene@reneterstegen.eu>
2025-08-08 14:18:53 +0200

    Services.yaml was not loaded. Now it should load

--- src/Messenger/CloudEvents/CloudEventsBundle.php
+++ src/Messenger/CloudEvents/CloudEventsBundle.php
@@ -3,12 +3,21 @@
 namespace Stegeman\Messenger\CloudEvents;
 
 use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class CloudEventsBundle extends AbstractBundle
 {
-    public function configure(DefinitionConfigurator $definition): void
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
+        $loader = new YamlFileLoader(
+            $builder,
+            new FileLocator(__DIR__ . DIRECTORY_SEPARATOR . 'config')
+        );
 
+        $loader->load('services.yaml');
     }
 }
